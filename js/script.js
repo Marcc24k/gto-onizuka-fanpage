272d156e328080d2aac7ea6653b50422ec51b8bb
Marc Cook <marcc24k@gmail.com>
2016-10-09 00:39:07 -0400

    Finish responive layout and add some new styling

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+    $( "" ).draggable();
+  });
+
+$(function() {
+    $( document ).tooltip();
+  });
---
+++ js/typed.js
@@ -0,0 +1,7 @@
+ 
+  $(function(){
+      $(".p").typed({
+        strings: ["First sentence.", "Second sentence."],
+        typeSpeed: 0
+      });
+  });
